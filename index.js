15de102658992f4e9454e158f6c2969a120199a9
LucasTiepoloo <lucas.tiepolo@escola.pr.gov.br>
2026-05-07 23:45:29 +0000

    primeiro commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const rotasItens = require('./routes/itens');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use('/itens', rotasItens);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
